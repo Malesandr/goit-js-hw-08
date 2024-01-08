e8fac24465fd06396327e76f028cdf34ba24ee8a
Malesandr <aleksandr.mikolenko@gmail.com>
2024-01-08 22:26:33 +0200

    first-hw-8

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const category = document.querySelectorAll(".item");
-console.log(`Number of categories: ${category.length}`);
-
-category.forEach((item) => {
-  console.log(`Category: ${item.querySelector("h2").textContent}`);
-  console.log(`Elements: ${item.querySelectorAll("ul li").length}`);
-});
